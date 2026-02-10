29a0cc08e78fa5b5ed3e483f98355e0358ca2ad8
syedalisaad488 <syedalisaad488@gmail.com>
2026-02-11 01:51:26 +0500

    color

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -190,7 +190,7 @@
                             <div class="card-grid-news service-card-1 hover-up wow animate__animated animate__fadeIn"
                                 data-wow-delay=".0s">
                                 <div class="card-info-bottom">
-                                    <h5>Permanent Recruitment:</h5>
+                                    <h5 class="text-white">Permanent Recruitment:</h5>
                                     <p class="text-gray-200">
                                         Connecting organizations with top-tier talent for long-term roles.
                                     </p>
